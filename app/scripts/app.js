b27f785333096105e46035e2ef180b406e894449
mbarsila <mbarsila_kadiu@outlook.com>
2016-07-14 10:22:15 +0200

    modified path

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,37 +1,40 @@
 'use strict';
 
 /**
- * @ngdoc overview
- * @name phoneCatalogApp
- * @description
- * # phoneCatalogApp
- *
- * Main module of the application.
- */
+* @ngdoc overview
+* @name phoneCatalogApp
+* @description
+* # phoneCatalogApp
+*
+* Main module of the application.
+*/
 angular
-  .module('phoneCatalogApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'Homepage',
-    'PhoneList',
-    'phoneDetail'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/homepage.html',
-        controller: 'CarouselCtrl'
-
-      })
-      .when('/about', {
-        templateUrl: 'views/phones.html',
-        controller: 'phoneDetails'
-})
-      .otherwise({
-        redirectTo: '/'
-      });
+.module('phoneCatalogApp', [
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch',
+  'Homepage',
+  'PhoneList',
+  'phoneDetail'
+])
+.config(function ($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'views/homepage.html',
+    controller: 'CarouselCtrl'
+  })
+  .when('/about', {
+    templateUrl: 'views/phones.html',
+    controller: 'phoneDetails'
+  })
+  .when('/phoneDetails', {
+    templateUrl: 'views/phoneDetail.html',
+    // controller: 'phoneDetails'
+  })
+  .otherwise({
+    redirectTo: '/'
   });
+});
