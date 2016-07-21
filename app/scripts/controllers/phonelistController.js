63a61f780805a0a2a68c8898282022490f2de211
mbarsila <mbarsila_kadiu@outlook.com>
2016-07-21 14:30:42 +0200

    modified phonelist controller

--- app/scripts/controllers/phonelistController.js
+++ app/scripts/controllers/phonelistController.js
@@ -1,8 +1,6 @@
 angular.module('phoneDetail', [])
 .controller('phoneDetails', function($scope, $http) {
+  $scope.sortColumn='age';
   $http.get("../../json/json/phones.json")
   .success(function(response) {$scope.names = response;});
- $scope.filterType= ["Newest", "Alphabetical", "Price"];
- //images
- $scope.images=[{image: '../../json/images/phones/motorola-xoom-with-wi-fi.0.jpg'}]
   });
